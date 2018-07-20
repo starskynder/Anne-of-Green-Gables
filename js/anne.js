942832596c42f38d03c59e795d49e42f9a69f801
starskynder <24276076+starskynder@users.noreply.github.com>
2018-07-20 14:29:26 +0200

    Comment code

--- js/anne.js
+++ js/anne.js
@@ -1,16 +1,27 @@
+// Select hamburger menu
 const menu = document.querySelector(".js-menu");
 
+// Select nav tag
 const nav = document.querySelector("nav");
+
+// Select navigation ul
 const navigation = document.querySelector(".nav");
 
+// Select navigation items
 const navItems = document.querySelectorAll(".nav__item");
 
+// Select hamburger's components
 const hamburger = document.querySelectorAll(".nav__hamburger");
 
+//Select hero text
 const heroTxt = document.querySelector(".hero__txt");
 
+//Listen for click on hamburger menu
 menu.addEventListener("click", openNav);
 
+
+//If hamburger manu is clicked toggle full navigation and transform hamburger 
+// icon in a "close" cross, also toggle hero text
 function openNav() {
     menu.classList.toggle("nav__menu--variant");
     heroTxt.classList.toggle("js--hidden");
